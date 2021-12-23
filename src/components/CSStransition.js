faea32869394428f2bc4bdfda63ab973c2156d4f
Kimihito Tanaka <kimi@kmd.keio.ac.jp>
2021-12-23 18:05:28 +0900

    transition#3: using CSSTransition

--- src/components/CSStransition.js
+++ src/components/CSStransition.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
+import { CSSTransition } from 'react-transition-group';
+
 
 import '../css/App.css';
 
 class Fade extends Component{
+    state = {
+        show:true
+    }
+
+    showDiv = () => {
+        this.setState({ show: !this.state.show ? true : false})
+    }
+
     render(){
         return(
-            <div>
-                CSS TRANSITIONS
-            </div>
+            <>
+                <CSSTransition
+                    in={this.state.show}
+                    timeout={5000}
+                    classNames="square"
+                >
+                    <div className={`square ${this.state.show}`}>Hello</div>
+                </CSSTransition>
+                <div className="showDiv" onClick={this.showDiv}>
+                    Show / Hide
+                </div>
+            </>
         )
     }
 }
