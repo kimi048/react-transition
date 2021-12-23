d426ef47196806d78dc9418924310f720f5f3bc0
Kimihito Tanaka <kimi@kmd.keio.ac.jp>
2021-12-23 16:16:48 +0900

    transition#1: using css

--- src/components/Transition.js
+++ src/components/Transition.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
+import { Transition } from 'react-transition-group';
 import '../css/App.css';
 
 class TransitionComp extends Component{
+    state = {
+        show:true
+    }
+
+    showDiv = () => {
+        this.setState({ show: !this.state.show ? true : false})
+    }
 
     render(){
         return(
-            <div>
-                Transition
-            </div>
+            <>
+                <Transition
+                    in={ this.state.show }
+                    timeout={ 2000 }
+                >
+                    {state =>
+                        
+                        <div style={{
+                            background: "red",
+                            height: "100px",
+                            transition: "all 2s ease",
+                            opacity: state ==="exited" || state==="exiting" ? 0 : 1
+                        }}>
+                            { state}
+                        </div>
+                    }
+                    
+                </Transition>
+                <div className="showDiv" onClick={this.showDiv}>
+                    Show / Hide
+                </div>
+            </>
 
         )
     }
